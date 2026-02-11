d3cf01127dc1b99a203faea1fd9f93ad77d2592f
Darwin Johansen <darwinjohansen06@gmail.com>
2026-02-11 19:16:46 +0300

    Initial Spring Boot project

---
+++ src/main/kotlin/com/kgamt/menu/MenuApplication.kt
@@ -0,0 +1,11 @@
+package com.kgamt.menu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenuApplication
+
+fun main(args: Array<String>) {
+	runApplication<MenuApplication>(*args)
+}
